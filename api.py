80bcb6663bc4ce3e20c02198f9937e986ece3b48
smashflashtech <smash.flash.tech@gmail.com>
2020-12-10 11:55:27 -0800

    all get routes working

--- api.py
+++ api.py
@@ -107,4 +107,4 @@ def abmts(set):
   SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
   json_url = os.path.join(SITE_ROOT,'stimuli/', f"set{set}.json")
   data = json.load(open(json_url))
-  return jsonify(abmts=data)
+  return jsonify(abmts=data) 

3092036dfff5c67bee89b2beb8d46d49b1325641
smashflashtech <smash.flash.tech@gmail.com>
2020-12-10 11:17:21 -0800

    adds get routes for all mvp json files

--- api.py
+++ api.py
@@ -23,5 +23,53 @@ def dct():
   SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
   json_url = os.path.join(SITE_ROOT,'stimuli/', 'dct.json')
   data = json.load(open(json_url))
-  print(data)
   return jsonify(dct=data)
+
+@app.route('/evott', methods=['GET'])
+def evott():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT,'stimuli/', 'evotTact.json')
+  data = json.load(open(json_url))
+  return jsonify(evott=data)
+
+@app.route('/evotiv', methods=['GET'])
+def evotiv():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT,'stimuli/', 'evotIV.json')
+  data = json.load(open(json_url))
+  return jsonify(evotiv=data)
+
+@app.route('/npst', methods=['GET'])
+def npst():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT,'stimuli/', 'novelPST.json')
+  data = json.load(open(json_url))
+  return jsonify(npst=data)
+
+@app.route('/sd', methods=['GET'])
+def sd():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT,'stimuli/', 'sDisc.json')
+  data = json.load(open(json_url))
+  return jsonify(sd=data)
+
+@app.route('/sp', methods=['GET'])
+def sp():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT,'stimuli/', 'sPair.json')
+  data = json.load(open(json_url))
+  return jsonify(sp=data)
+
+@app.route('/tat', methods=['GET'])
+def tat():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT,'stimuli/', 'talkAloud.json')
+  data = json.load(open(json_url))
+  return jsonify(tat=data)
+
+@app.route('/tsf', methods=['GET'])
+def tsf():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT,'stimuli/', 'tsf.json')
+  data = json.load(open(json_url))
+  return jsonify(tsf=data)

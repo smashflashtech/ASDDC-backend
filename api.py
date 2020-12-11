73ecbb31017e2eeeaf5561ed31d10311da8a19af
smashflashtech <smash.flash.tech@gmail.com>
2020-12-10 17:30:06 -0800

    corrects spelling of namts

--- api.py
+++ api.py
@@ -103,8 +103,8 @@ def amts():
   return jsonify(amts=data)
 
 @app.route('/nonamts/<set>', methods=['GET'])
-def abmts(set):
+def namts(set):
   SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
   json_url = os.path.join(SITE_ROOT,'stimuli/', f"set{set}.json")
   data = json.load(open(json_url))
-  return jsonify(abmts=data) 
+  return jsonify(namts=data) 

31bdfc2bf356e960a98d0e112d41124ede0b5ba1
smashflashtech <smash.flash.tech@gmail.com>
2020-12-16 00:34:14 -0800

    adds group id to participants

--- seed.py
+++ seed.py
@@ -16,6 +16,7 @@ groups.append(Group(
 # db.session.commit() # commits the changes to the database
 
 participants = [Participant( 
+  group_id = 1,
   dyad_L = 'A',
   dyad_N = 1,
   remedial = 'nope',
@@ -23,28 +24,33 @@ participants = [Participant(
   notes = 'Test'
 )]
 participants.append(Participant(
+  group_id = 1,
   dyad_L = 'A',
   dyad_N = 2,
   date_of_participation = '2020-12-15; 10:33',
   notes = 'Test'
 ))
 participants.append(Participant(
+  group_id = 2,
   dyad_L = 'I',
   dyad_N = 1,
   remedial = 'yep',
   date_of_participation = '2020-12-15; 10:33'
 ))
 participants.append(Participant(
+  group_id = 2,
   dyad_L = 'I',
   dyad_N = 2,
   date_of_participation = '2020-12-15; 10:33',
 ))
 participants.append(Participant(
+  group_id = 3,
   dyad_L = 'Q',
   dyad_N = 1,
   date_of_participation = '2020-12-15; 10:33'
 ))
 participants.append(Participant(
+  group_id = 3,
   dyad_L = 'Q',
   dyad_N = 2,
   date_of_participation = '2020-12-15; 10:33'
